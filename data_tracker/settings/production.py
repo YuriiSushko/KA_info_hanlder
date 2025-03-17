07ce12947842944f3503b46c3813358cae5570f4
yurii_sushko <ysushko@kse.org.ua>
2025-03-17 17:43:02 +0200

    ahh

--- data_tracker/settings/production.py
+++ data_tracker/settings/production.py
@@ -2,7 +2,7 @@ from data_tracker.settings.base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ka-info-handler.com', 'ka-info-handler.onrender.com']
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 print(BASE_DIR)
 
 DATABASES = {
