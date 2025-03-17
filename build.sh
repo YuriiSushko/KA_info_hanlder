b2fea7af73f620f6ce4ebd4f6ab75d965d1095a3
yurii_sushko <ysushko@kse.org.ua>
2025-03-17 17:38:58 +0200

    test

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
+python manage.py shell -c "from django.conf import settings; print('BASE_DIR:', settings.BASE_DIR)"
 
 mkdir -p /opt/render/project/src/data_tracker/staticfiles/
 
