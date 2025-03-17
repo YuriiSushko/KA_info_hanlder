dd8bea0abedd9a94bd8961523e998cb713b9b2d6
yurii_sushko <ysushko@kse.org.ua>
2025-03-17 18:19:56 +0200

    build

--- build.sh
+++ build.sh
@@ -2,14 +2,21 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Print Base Directory (for debugging)
 python manage.py shell -c "from django.conf import settings; print('BASE_DIR:', settings.BASE_DIR)"
 
+# Manually create the staticfiles directory (Render might not create it automatically)
 mkdir -p /opt/render/project/src/staticfiles
+echo "Created staticfiles directory"
 
-# Convert static asset files
+# Collect static files
 python manage.py collectstatic --noinput
+echo "Collected static files"
 
-# Apply any outstanding database migrations
+# Apply migrations
 python manage.py migrate
+echo "Applied migrations"
