e44d35e7bc98468eae19efc062c83a62be869746
yurii_sushko <ysushko@kse.org.ua>
2025-03-17 17:50:50 +0200

    auuuh

--- data_tracker/settings/production.py
+++ data_tracker/settings/production.py
@@ -2,8 +2,14 @@ from data_tracker.settings.base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ka-info-handler.com', 'ka-info-handler.onrender.com']
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-print(BASE_DIR)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 DATABASES = {
     'default': {
@@ -24,10 +30,10 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True  # Redirect all HTTP requests to HTTPS
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-# Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
 # and renames the files with unique names for each version to support long-term caching
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
