14ee3dbecfcfff7f244d8eaca61fd8339f5cc267
yurii_sushko <ysushko@kse.org.ua>
2025-03-17 16:14:41 +0200

    preparing for render

--- data_tracker/settings/production.py
+++ data_tracker/settings/production.py
@@ -9,8 +9,15 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ka-info-handler.com', 'ka-info-handl
 # In production.py
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.NullStorage'
 
-
 # Security settings for production
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True  # Redirect all HTTP requests to HTTPS
+
+STATIC_URL = '/static/'
+
+# Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+# and renames the files with unique names for each version to support long-term caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
