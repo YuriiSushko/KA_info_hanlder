d6883c1db15519f6cee39498cc40e05b8ded5218
yurii_sushko <ysushko@kse.org.ua>
2025-03-18 10:37:58 +0200

    redundunt

--- build.sh
+++ build.sh
@@ -11,10 +11,6 @@ pip install -r requirements.txt
 # # Debug: Print Base Directory
 # python manage.py shell -c "from django.conf import settings; print('BASE_DIR:', settings.BASE_DIR)"
 
-# Debug: Create staticfiles directory explicitly
-mkdir -p /opt/render/project/src/staticfiles
-echo "✅ Created staticfiles directory"
-
 # Run collectstatic and check for errors
 echo "Running collectstatic..."
 python manage.py collectstatic --noinput --ignore=node_modules --ignore=*.mp4 --ignore=*.zip || { echo "❌ collectstatic failed"; exit 1; }
