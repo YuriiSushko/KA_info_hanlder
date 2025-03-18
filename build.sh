666754fdc280a78bc70e5ae82990c83bd90dfcc0
yurii_sushko <ysushko@kse.org.ua>
2025-03-18 11:49:35 +0200

    verbose

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ ls /opt/render/project/src/data_tracker/staticfiles/
 
 # Run collectstatic and check for errors
 echo "Running collectstatic..."
-python manage.py collectstatic --noinput || { echo "❌ collectstatic failed"; exit 1; }
+python manage.py collectstatic --noinput --verbosity 2 || { echo "❌ collectstatic failed"; exit 1; }
 echo "✅ Collected static files"
 
 # Apply database migrations
